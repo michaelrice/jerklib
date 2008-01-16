d79a38800ed77af663565ea4768f6a6bec6d9b69
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-16 06:13:32 +0000

    Quit->QuitOperation; ChannelManager->ChannelManagerOperation
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@33 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/examples/jerkbot2/jerkbot/operations/ChannelManagerOperation.java
@@ -0,0 +1,33 @@
+package jerklib.examples.jerkbot2.jerkbot.operations;
+
+import jerklib.Channel;
+import jerklib.events.IRCEvent;
+import jerklib.events.ChannelMsgEvent;
+
+import java.util.List;
+
+/**
+ * This plugin illustrates how to use the channel-related events.
+ * @author Robert O'Connor <robby.oconnor@gmail.com>
+ */
+public class ChannelManagerOperation implements BotOperation {
+   
+    public ChannelManagerOperation() {
+    }
+
+    public void handleMessage(IRCEvent e) {
+         if (e.getType() == IRCEvent.Type.CHANNEL_MESSAGE) {
+            ChannelMsgEvent event = (ChannelMsgEvent) e;
+            String msg = event.getMessage(); // the message
+            if (msg.startsWith("~users")) {
+                StringBuilder sb = new StringBuilder();
+                Channel c = event.getChannel();
+                List<String> users = c.getNicks(); // users in the channel.
+                for (String user : users) {
+                    sb.append(user).append(" "); 
+                }
+                e.getSession().channelSay(c.getName(), "Users in " + c.getName() + " are: " + sb.toString());
+            }
+        }
+    }
+}
---
+++ jerklib/src/jerklib/examples/jerkbot2/jerkbot/operations/QuitOperation.java
@@ -0,0 +1,44 @@
+package jerklib.examples.jerkbot2.jerkbot.operations;
+
+
+import jerklib.events.IRCEvent;
+import jerklib.events.PrivateMsgEvent;
+
+/**
+ * This will allow you to make the bot quit via IRC.
+ * It illustrates how to use PrivateMsgEvent.
+ * @author Robert O'Connor <robby.oconnor@gmail.com>
+ */
+public class QuitOperation implements BotOperation {
+    private String password;
+
+    public QuitOperation(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void handleMessage(IRCEvent e) {
+        if(e.getType() == IRCEvent.Type.PRIVATE_MESSAGE) {
+            // cast to the real type
+            PrivateMsgEvent event = (PrivateMsgEvent)e;
+            String msg = event.getMessage(); // message
+            if(msg.startsWith("~quit")) {
+                if(msg.substring("~quit ".length()).equals(password)) {
+                    e.getSession().close("I was asked to leave");
+                    System.exit(0); 
+                }
+            }
+
+            
+
+        }
+    }
+}
+
