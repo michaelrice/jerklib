c0f42c7ef956432c0b9d22bf7dda826c4f28991e
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-16 02:10:42 +0000

    Check in my example -- also named jerkbot -- we're very creative you know!
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@5 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/examples/jerkbot2/src/jerkbot/operations/BotOperation.java
@@ -0,0 +1,14 @@
+package jerkbot.operations;
+
+import jerklib.events.IRCEvent;
+
+
+public interface BotOperation {
+
+    /**
+     * This method is responsible for differentiating between whether
+     * or not the bot was addressed.
+     * 
+     */
+    public void handleMessage(IRCEvent e);
+}
