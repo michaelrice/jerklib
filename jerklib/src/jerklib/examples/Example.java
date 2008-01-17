1c4a9ebf07d30245125cc0be06f120020ef6e8a5
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-17 01:07:10 +0000

    change the String displayed to the user via stdout for the User Name field. it no longer says 'Login'
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@50 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/Example.java
+++ jerklib/src/jerklib/examples/Example.java
@@ -54,7 +54,7 @@ public class Example implements IRCEventListener
 			System.out.println("Good " +e.getRawEventData());
             ChannelMsgEvent event = (ChannelMsgEvent)e;
             System.out.println("Nick: "+event.getNick());
-            System.out.println("Login/Ident: "+event.getUserName());
+            System.out.println("User Name/Ident: "+event.getUserName());
             System.out.println("Host name: "+event.getHostName());
             System.out.println("Channel Name: "+event.getChannel().getName());
             System.out.println("Message: "+event.getMessage());
@@ -64,7 +64,7 @@ public class Example implements IRCEventListener
 			System.out.println("Good Prv " +e.getRawEventData());
             PrivateMsgEvent event = (PrivateMsgEvent)e;
             System.out.println("Nick Host: "+event.getHostName());
-            System.out.println("Login/Ident: "+event.getUserName());
+            System.out.println("User Name/Ident: "+event.getUserName());
         }
 		else if(e.getType() == IRCEvent.Type.MODE_EVENT)
 		{
