5fd2d4ac3f5cc3100ada721896b1a6e2c41836c0
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-18 02:44:45 +0000

    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@92 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/Example.java
+++ jerklib/src/jerklib/examples/Example.java
@@ -4,14 +4,7 @@ import jerklib.ConnectionManager;
 import jerklib.ProfileImpl;
 import jerklib.Session;
 import jerklib.events.IRCEvent;
-import jerklib.events.ModeEvent;
-import jerklib.events.TopicEvent;
-import jerklib.events.PrivateMsgEvent;
-import jerklib.events.ChannelMsgEvent;
-import jerklib.events.InviteEvent;
-import jerklib.events.JoinCompleteEvent;
 import jerklib.events.listeners.IRCEventListener;
-
 
 
 public class Example implements IRCEventListener
@@ -38,57 +31,11 @@ public class Example implements IRCEventListener
 		}
 		else if(e.getType() == IRCEvent.Type.READY_TO_JOIN)
 		{
-			//e.getSession().whois("mohadib");
-            e.getSession().joinChannel("#jerklib");
-            /*e.getSession().joinChannel("#ubuntu");
-			e.getSession().joinChannel("#debian");
-			e.getSession().joinChannel("#perl");*/
-            //e.getSession().rawSay("LIST ##swing");
-        }
-		else if(e.getType() == IRCEvent.Type.JOIN_COMPLETE)
-		{
-            JoinCompleteEvent event = (JoinCompleteEvent)e;
-            e.getSession().channelSay(event.getChannel().getName(),"Hai 2u");
-        }
-        else if(e.getType() == IRCEvent.Type.INVITE_EVENT) {
-            final InviteEvent event = (InviteEvent)e;
-            e.getSession().joinChannel(event.getChannel());
-        }
-        else if(e.getType() == IRCEvent.Type.TOPIC)
-		{
-			TopicEvent te = (TopicEvent)e;
-            System.out.println("Topic for " + te.getChannel().getName() + " " + te.getTopic() + " set by " + te.getSetBy() + " set when:" + te.getSetWhen());
+			e.getSession().channelList();
 		}
-		else if(e.getType() == IRCEvent.Type.CHANNEL_MESSAGE)
+		else if(e.getType() == IRCEvent.Type.CHANNEL_LIST_EVENT)
 		{
-			System.out.println("Good Chan Msg: " +e.getRawEventData());
-            ChannelMsgEvent event = (ChannelMsgEvent)e;
-            System.out.println("Nick: "+event.getNick());
-            System.out.println("User Name: "+event.getUserName());
-            System.out.println("Host Name: "+event.getHostName());
-            System.out.println("Channel Name: "+event.getChannel().getName());
-            System.out.println("Message: "+event.getMessage());
-            if(event.getMessage().startsWith("~say")) {
-                e.getSession().channelSay(event.getChannel().getName(),event.getMessage().substring("~say".length()));
-            }
-            else if(event.getMessage().startsWith("~join")) {
-                e.getSession().joinChannel(event.getMessage().substring("~join ".length()));
-            }
-        }
-		else if(e.getType() == IRCEvent.Type.PRIVATE_MESSAGE)
-		{
-			System.out.println("Good Prv: " +e.getRawEventData());
-            PrivateMsgEvent event = (PrivateMsgEvent)e;
-            System.out.println("Host Name: "+event.getHostName());
-            System.out.println("User Name: "+event.getUserName());
-        }
-		else if(e.getType() == IRCEvent.Type.MODE_EVENT)
-		{
-			ModeEvent me = (ModeEvent)e;
-			if(me.getUser().equals(e.getSession().getNick()) && me.getMode().equalsIgnoreCase("+o"))
-			{
-				me.getChannel().setTopic("A WINNAR IS ME");
-			}
+			System.out.println(e.getRawEventData());
 		}
 	}
 	
