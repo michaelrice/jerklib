80aefd7ea0a2c216a62c409e448f2d5e0e402094
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-18 00:34:22 +0000

    mohadib got the right 323 response regex :-D
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@77 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/util/misc/RegexTester.java
+++ jerklib/src/jerklib/util/misc/RegexTester.java
@@ -25,7 +25,7 @@ public class RegexTester {
         m = null;
         // check for end of /list
         //^:(.*?)\\!\\S+\\s+KICK\\s+(\\S+)\\s+(\\S+)\\s+:?(.*)
-        p = Pattern.compile("^:.*+\\s323+$"); 
+        p = Pattern.compile("^:\\S+\\s323\\s\\S+\\s:.*$");
         m = p.matcher(":card.freenode.net 323 r0bby___ :End of /LIST");
 
         // does it match?!?!?
