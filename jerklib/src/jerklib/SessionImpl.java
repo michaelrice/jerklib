d93b247f79f5c895e38f08b9c231eaa1cf47b10d
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-23 21:37:44 +0000

    add @see javadoc tag
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@234 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/SessionImpl.java
+++ jerklib/src/jerklib/SessionImpl.java
@@ -504,6 +504,7 @@ public class SessionImpl implements Session
 
     /**
      * This method returns the last message used when the user set themselves away.
+     * @see Session#getPreviousAwayMsg()
      * @return the last away message used.
      */
     @Override
