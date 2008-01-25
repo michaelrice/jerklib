a4038bce8f08ac605cef972574997cc02f9dd8b2
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-25 07:08:44 +0000

    removing un-used imports
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@256 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/jerkbot/Jerkbot.java
+++ jerklib/src/jerklib/examples/jerkbot/Jerkbot.java
@@ -24,6 +24,7 @@ import java.util.Random;
 
 /**
  * This an the main class and entry point.
+ *
  * @author Robert O'Connor <robby.oconnor@gmail.com>
  */
 public class Jerkbot implements IRCEventListener {
@@ -40,8 +41,8 @@ public class Jerkbot implements IRCEventListener {
      * @param port   the port of the server to connect to.
      */
     public Jerkbot(String nick, String login, String server, int port) {
-        Profile profile = new ProfileImpl(login, nick, nick+new Random().nextInt(42),
-                nick+ new Random().nextInt(512));
+        Profile profile = new ProfileImpl(login, nick, nick + new Random().nextInt(42),
+                nick + new Random().nextInt(512));
         ConnectionManager manager = new ConnectionManager(profile);
         manager.requestConnection(server, port, profile).addIRCEventListener(this);
     }
@@ -52,7 +53,7 @@ public class Jerkbot implements IRCEventListener {
         operations.add(new SayOperation());
         operations.add(new JoinOperation());
         operations.add(new PartOperation());
-        operations.add(new AwayOperation());        
+        operations.add(new AwayOperation());
     }
 
 
@@ -62,42 +63,43 @@ public class Jerkbot implements IRCEventListener {
      * @param e the event we receieved.
      */
     public void recieveEvent(IRCEvent e) {
-        for(BotOperation operation : operations) {
+        for (BotOperation operation : operations) {
             operation.handleMessage(e);
         }
         if (e instanceof ConnectionCompleteEvent) {
             e.getSession().setAway("I am a bot and you fail at life.");
             e.getSession().joinChannel("#jerklib");
             e.getSession().joinChannel("#jerklib2");
+            e.getSession().joinChannel("#gayetc");
         } else if (e instanceof JoinCompleteEvent) {
             JoinCompleteEvent event = (JoinCompleteEvent) e;
-            event.getChannel().say("Hai 2u");           
-            event.getChannel().say("I am running jerklib version "+ConnectionManager.getVersion());            
-        } else if(e instanceof JoinEvent) {
-            JoinEvent event = (JoinEvent)e;
-            e.getSession().channelSay(event.getChannel().getName(),"Hey "+event.getWho()+
-                    " your hostname is "+event.getHostName());
-        } else if(e instanceof KickEvent) {
-            KickEvent event = (KickEvent)e;
-            System.out.println("By Who: "+event.byWho());
-            System.out.println("User Name: "+event.getUserName());
-            System.out.println("Host Name: "+event.getHostName());
-            System.out.println("Victim: "+event.getWho());
-            System.out.println("Kick Msg: "+event.getMessage());
-            System.out.println("Channel: "+event.getChannel().getName());
+            event.getChannel().say("Hai 2u");
+            event.getChannel().say("I am running jerklib version " + ConnectionManager.getVersion());
+        } else if (e instanceof JoinEvent) {
+            JoinEvent event = (JoinEvent) e;
+            e.getSession().channelSay(event.getChannel().getName(), "Hey " + event.getWho() +
+                    " your hostname is " + event.getHostName());
+        } else if (e instanceof KickEvent) {
+            KickEvent event = (KickEvent) e;
+            System.out.println("By Who: " + event.byWho());
+            System.out.println("User Name: " + event.getUserName());
+            System.out.println("Host Name: " + event.getHostName());
+            System.out.println("Victim: " + event.getWho());
+            System.out.println("Kick Msg: " + event.getMessage());
+            System.out.println("Channel: " + event.getChannel().getName());
 
-        } else if(e instanceof AwayEvent) {
-            AwayEvent event = (AwayEvent)e;
-            System.out.println("Nick: "+event.getNick());
-            System.out.println("Event Type: "+event.getEventType());
-            System.out.println("Is Away: "+event.isAway());
-            System.out.println("Is You: "+event.isYou());
-            System.out.println("Message: "+event.getAwayMessage());
+        } else if (e instanceof AwayEvent) {
+            AwayEvent event = (AwayEvent) e;
+            System.out.println("Nick: " + event.getNick());
+            System.out.println("Event Type: " + event.getEventType());
+            System.out.println("Is Away: " + event.isAway());
+            System.out.println("Is You: " + event.isYou());
+            System.out.println("Message: " + event.getAwayMessage());
 
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         Jerkbot bot = new Jerkbot("jerkbot", "jerkbot", "irc.freenode.org", 6667);
         bot.loadOperations();
     }
