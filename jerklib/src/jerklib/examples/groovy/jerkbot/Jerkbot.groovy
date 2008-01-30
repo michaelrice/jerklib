1b3df90d0cd77e912e68174ebcf46f6d0f2af537
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-30 07:52:17 +0000

    It's a very groovy jerkbot!!!
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@272 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/examples/groovy/jerkbot/Jerkbot.groovy
@@ -0,0 +1,39 @@
+package jerklib.examples.groovy
+
+import jerklib.Profile
+import jerklib.ProfileImpl
+import jerklib.events.listeners.IRCEventListener
+import jerklib.events.IRCEvent
+import jerklib.events.JoinCompleteEvent
+
+/**
+* Created: Jan 29, 2008 11:42:23 PM
+* @author <a href="mailto:robby.oconnor@gmail.com">Robert O'Connor</a>
+*/
+class Jerkbot implements IRCEventListener {
+    def jerklib.ConnectionManager manager;
+
+    Jerkbot(String nick, String username, String hostname, int port) {
+       def Profile profile = new ProfileImpl(username, nick, nick + new Random().nextInt(42),
+                nick + new Random().nextInt(512))
+       manager = new jerklib.ConnectionManager(profile)
+        manager.requestConnection(hostname,port).addIRCEventListener(this)
+    }
+    
+
+    public static void main(String[] args) {
+        def Jerkbot bot = new Jerkbot("jerkbot","jerkbot","irc.freenode.org",6667)        
+    }
+
+    public void recieveEvent(IRCEvent e) {
+        if(e.getType() == IRCEvent.Type.CONNECT_COMPLETE) {
+            e.getSession().joinChannel("#jerklib")
+        }
+        else if(e.getType() == IRCEvent.Type.JOIN_COMPLETE) {
+            def JoinCompleteEvent event = (JoinCompleteEvent)e
+            e.getSession().channelSay(event.getChannel().getName(), "Hai 2u")
+        }
+
+    }
+
+}
