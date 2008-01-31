5efc2f4375fdd448a0bcc3f7382ef4f90f25409e
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-31 23:38:48 +0000

    beginning of WhoEvent to parse WHO
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@296 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/events/WhoEvent.java
@@ -0,0 +1,46 @@
+package jerklib.events;
+
+/**
+ * Created: Jan 31, 2008 6:19:28 PM
+ *
+ * @author <a href="mailto:robby.oconnor@gmail.com">Robert O'Connor</a>
+ */
+public interface WhoEvent extends IRCEvent {
+    /**
+     * Get the nick of the user
+     * @return the nick of the user.
+     */
+    public String getNick();
+
+    /**
+     * Get the username of the user
+     * @return the username
+     */
+    public String getUserName();
+
+    /**
+     * Get the hostname of the user
+     * @return the hostname
+     */
+    public String getHostName();
+
+    /**
+     * Get the real name of the user.
+     * @return the real name
+     */
+    public String getRealName();
+
+    /**
+     * Retrieve the channel (for when you WHO a channel)
+     * @return the channel or an empty String
+     */
+    public String getChannel();
+
+    /**
+     * Get whether or not the user is away.
+     * @return whether or not the user is away.
+     */
+    public boolean isAway();
+
+
+}
