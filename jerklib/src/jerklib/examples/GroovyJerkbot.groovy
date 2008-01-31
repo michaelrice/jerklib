d59dffc62a857e9f7f9dd930d68ac533714de465
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-01-31 09:26:44 +0000

    add some more
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@293 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/GroovyJerkbot.groovy
+++ jerklib/src/jerklib/examples/GroovyJerkbot.groovy
@@ -1,4 +1,4 @@
-package jerklib.examples 
+package jerklib.examples
 
 import jerklib.Profile
 import jerklib.ProfileImpl
@@ -7,6 +7,7 @@ import jerklib.events.IRCEvent
 import jerklib.events.JoinCompleteEvent
 import jerklib.ConnectionManager
 import jerklib.events.ChannelMsgEvent
+import jerklib.events.PrivateMsgEvent
 
 /**
 * Created: Jan 29, 2008 11:42:23 PM
@@ -16,30 +17,37 @@ class GroovyJerkbot implements IRCEventListener {
     def ConnectionManager manager
 
     GroovyJerkbot(String nick, String username, String hostname, int port) {
-       def Profile profile = new ProfileImpl(username, nick, nick + new Random().nextInt(42),
+        def Profile profile = new ProfileImpl(username, nick, nick + new Random().nextInt(42),
                 nick + new Random().nextInt(512))
-       manager = new ConnectionManager(profile)
-       manager.requestConnection(hostname,port).addIRCEventListener(this)
-    }    
+        manager = new ConnectionManager(profile)
+        manager.requestConnection(hostname, port).addIRCEventListener(this)
+    }
 
     public static void main(String[] args) {
-        def GroovyJerkbot bot = new GroovyJerkbot("jerkbot","jerkbot","irc.freenode.org",6667)
+        def GroovyJerkbot bot = new GroovyJerkbot("jerkbot", "jerkbot", "irc.freenode.org", 6667)
     }
 
     public void recieveEvent(IRCEvent e) {
-        if(e.getType() == IRCEvent.Type.CONNECT_COMPLETE) {
+        if (e.getType() == IRCEvent.Type.CONNECT_COMPLETE) {
             e.getSession().joinChannel("#jerklib")
         }
-        else if(e.getType() == IRCEvent.Type.JOIN_COMPLETE) {
-            def JoinCompleteEvent event = (JoinCompleteEvent)e
+        else if (e.getType() == IRCEvent.Type.JOIN_COMPLETE) {
+            def JoinCompleteEvent event = (JoinCompleteEvent) e
             e.getSession().channelSay(event.getChannel().getName(), "Hai 2u")
-        } else if(e.getType() == IRCEvent.Type.CHANNEL_MESSAGE) {
-            ChannelMsgEvent event = (ChannelMsgEvent)e;
-            // groovy has native regex support! 
+        } else if (e.getType() == IRCEvent.Type.CHANNEL_MESSAGE) {
+            ChannelMsgEvent event = (ChannelMsgEvent) e;
+            // what does this is take the channel msg and match it to the pattern ~say foo
             def matcher = event.getMessage() =~ /^~say\s+(.*)$/
-            println(matcher.matches());
-            if(matcher.matches()) {
-                e.getSession().channelSay(event.getChannel().getName(),matcher.group(1));
+            if (matcher.matches()) {
+                e.getSession().channelSay(event.getChannel().getName(), matcher.group(1));
+            }else if(event.getMessage() ==~ /^~part.*$/) {
+                e.getSession().partChannel(event.getChannel(),"I was asked to leave");
+            }
+
+        } else if (e.getType() == IRCEvent.Type.PRIVATE_MESSAGE) {
+            PrivateMsgEvent event = (PrivateMsgEvent)e;            
+            if(event.getMessage() ==~ /^~quit.*$/) {
+                e.getSession().close("I was asked to leave."); 
             }
         }
 
