9ee4d159f6116049eadee4c7d918c4c5f56fee8b
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-02-03 03:25:27 +0000

    reworded some suff in awayevent javadoc
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@342 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/events/AwayEvent.java
+++ jerklib/src/jerklib/events/AwayEvent.java
@@ -5,8 +5,8 @@ package jerklib.events;
  *  This is an event that is fired under three conditions:
  *  <ul>
  *      <li>Sending a message to a user who is marked as away.</li>
- *      <li>You mark yourself as away.</li>
- *      <li>You return from away.</li>
+ *      <li>User of lib marks self as away.</li>
+ *      <li>User of lib returns from away.</li>
  * </ul>
  * You can determine under which circumstance the event was fired by looking at
  * the {@link EventType}.</p> 
@@ -18,9 +18,9 @@ public interface AwayEvent extends IRCEvent {
 
     /**
      * An enum to determine the type of event that was fired.
-     * <br>WENT_AWAY is when we went go away.<br>
-     * RETURNED_FROM_AWAY is when we return from an away state.<br>
-     * USER_IS_AWAY is when a remote user goes away other than us.<br>
+     * <br>WENT_AWAY is when user of lib goes away.<br>
+     * RETURNED_FROM_AWAY is when user of lib returns from away state.<br>
+     * USER_IS_AWAY is when some other user goes away<br>
      */
     public static enum EventType {
         WENT_AWAY,
@@ -36,20 +36,20 @@ public interface AwayEvent extends IRCEvent {
     public EventType getEventType();
 
     /**
-     * Whether or not you or the user is away. 
+     * Whether or not subject of event is away.
      * @return if we're away or not.
      */
     public boolean isAway();
 
 
     /**
-     * Returns the away message or an empty String if it was you who caused the event to fire. 
+     * Returns the away message or an empty String if it was user of lib who caused the event to fire. 
      * @return the away message
      */
     public String getAwayMessage();
 
     /**
-     * Whether or not it was us that fired this event
+     * Whether or not it was user of lib that caused this event
      * @return if it was us or not.
      */
     public boolean isYou();
