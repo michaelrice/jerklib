6ebcedd7d2af759fa26803506451a8000b331f73
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-02-04 21:47:03 +0000

    this shall be deleted.
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@388 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/examples/GroovyJerkbot.java
@@ -0,0 +1,93 @@
+package jerklib.examples.jerkbot
+
+import jerklib.Profile
+import jerklib.ProfileImpl
+import jerklib.events.listeners.IRCEventListener
+import jerklib.events.IRCEvent
+import jerklib.events.JoinCompleteEvent
+import jerklib.ConnectionManager
+import jerklib.events.ChannelMsgEvent
+import jerklib.events.PrivateMsgEvent
+import jerklib.events.NickChangeEvent
+import jerklib.events.AwayEvent
+
+
+
+/**
+* Created: Jan 29, 2008 11:42:23 PM
+* @author <a href="mailto:robby.oconnor@gmail.com">Robert O'Connor</a>
+*/
+class GroovyJerkbot implements IRCEventListener {
+    def ConnectionManager manager
+
+    GroovyJerkbot(String nick, String username, String hostname, int port) {
+        def Profile profile = new ProfileImpl(username, nick, nick + new Random().nextInt(42),
+                nick + new Random().nextInt(512))
+        manager = new ConnectionManager(profile)
+        manager.requestConnection(hostname, port).addIRCEventListener(this) 
+
+    }
+
+   
+    def static void main(String[] args) {
+        def GroovyJerkbot bot = new GroovyJerkbot("jerkbot", "jerkbot", "irc.freenode.org", 6667)
+    }
+                            
+    @Override
+    def void receiveEvent(IRCEvent e) {
+        if (e.getType() == IRCEvent.Type.CONNECT_COMPLETE) {
+            e.getSession().joinChannel("#jerklib")
+            e.getSession().who("mohadib")
+            e.getSession().setAway("FUCK!")
+        }
+        else if (e.getType() == IRCEvent.Type.JOIN_COMPLETE) {
+            def JoinCompleteEvent event = (JoinCompleteEvent) e
+            e.getSession().channelSay(event.getChannel().getName(), "Hai 2u")
+        } else if (e.getType() == IRCEvent.Type.CHANNEL_MESSAGE) {
+            ChannelMsgEvent event = (ChannelMsgEvent) e
+            // what does this is take the channel msg and match it to the pattern ~say foo
+            def matcher = event.getMessage() =~ /^~say\s+(.*)$/
+            if (matcher.matches())
+            {
+                e.getSession().channelSay(event.getChannel().getName(), matcher.group(1))
+            }else if(event.getMessage() ==~ /^~part.*$/) {
+                e.getSession().partChannel(event.getChannel(),"I was asked to leave")
+            } else {
+                def whoMatcher = event.getMessage() =~ /^~who\s+(.*)$/
+                if(whoMatcher.matches()) {
+                    e.getSession().who(whoMatcher.group(1))
+                }
+                def awayMatcher = event.getMessage() =~ /^~away\s+(.*)$/
+                if(awayMatcher.matches()) {
+                    e.getSession().setAway(m.group(1))
+                }else {
+                    e.getSession().unsetAway()
+                }
+            }
+
+        } else if (e.getType() == IRCEvent.Type.PRIVATE_MESSAGE) {
+            PrivateMsgEvent event = (PrivateMsgEvent)e
+            if(event.getMessage() ==~ /^~quit.*$/) {
+                e.getSession().close("I was asked to leave.")
+            }
+        } else if (e.getType() == IRCEvent.Type.NICK_CHANGE) {
+            NickChangeEvent event = (NickChangeEvent) e
+            println event.getOldNick()
+            println event.getNewNick()
+            println event.getUserName()
+            println event.getHostName()
+            println e.getRawEventData()
+        } else {
+            if (e.getType() == IRCEvent.Type.AWAY_EVENT) {
+                AwayEvent event = (AwayEvent)e
+                println "Nick: "+event.getNick()
+                println "Event Type: "+event.getEventType()
+                println "Us?: "+event.isYou()
+                println "Away?: "+event.isAway()
+
+            }
+        }
+
+    }
+
+}
