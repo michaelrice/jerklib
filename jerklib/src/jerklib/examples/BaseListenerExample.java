812384b10c7612d46a5e43c2aa22f518610956cb
epesh <epesh@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-10 05:24:05 +0000

    Nadie sabe los apuros que he visto. Nadie sabe mi dolor. Nadie sabe los apuros que he visto, nadie sabe solamente el individuo que tiene lavar mi coche. Su nombre es Eugene.
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@554 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/BaseListenerExample.java
+++ jerklib/src/jerklib/examples/BaseListenerExample.java
@@ -35,6 +35,10 @@ public class BaseListenerExample extends BaseListener implements Runnable {
         log.info(event.getChannel() + ":" + event.getNick() + ":" + event.getMessage());
         if ("now die".equalsIgnoreCase(event.getMessage())) {
             event.getChannel().say("Okay, fine, I'll die");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {                
+            }
             System.exit(0);
         }
     }
