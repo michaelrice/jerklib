0aa73f1b5b5c7cde58e89606785fd6637638be0f
epesh <epesh@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-10 04:02:25 +0000

    More event types.
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@544 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/events/listeners/BaseListener.java
+++ jerklib/src/jerklib/events/listeners/BaseListener.java
@@ -29,7 +29,43 @@ public abstract class BaseListener implements IRCEventListener {
         if(Type.ERROR.equals(t)) {
             handleErrorEvent((ErrorEvent)e);
         }
+        if(Type.INVITE_EVENT.equals(t)) {
+            handleInviteEvent((InviteEvent)e);
+        }
+        if(Type.JOIN.equals(t)) {
+            handleJoinEvent((JoinEvent)e);
+        }
+        if(Type.JOIN_COMPLETE.equals(t)) {
+            handleJoinCompleteEvent((JoinCompleteEvent)e);
+        }
+        if(Type.KICK_EVENT.equals(t)) {
+            handleKickEvent((KickEvent)e);
+        }
+        if(Type.MODE_EVENT.equals(t)) {
+            handleModeEvent((ModeEvent)e);
+        }
+        if(Type.MOTD.equals(t)) {
+            handleMotdEvent((MotdEvent)e);
+        }
+        
+    }
 
+    private void handleMotdEvent(MotdEvent event) {
+    }
+
+    private void handleModeEvent(ModeEvent event) {
+    }
+
+    private void handleKickEvent(KickEvent event) {
+    }
+
+    private void handleJoinCompleteEvent(JoinCompleteEvent event) {
+    }
+
+    private void handleJoinEvent(JoinEvent event) {        
+    }
+
+    private void handleInviteEvent(InviteEvent event) {
     }
 
     private void handleErrorEvent(ErrorEvent error) {
