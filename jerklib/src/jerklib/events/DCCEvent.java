093b39dc25a121a16ce1bde6541963771caec6f3
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-10 05:45:25 +0000

    get rid of this too for now
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@557 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/events/DCCEvent.java
+++
@@ -1,9 +0,0 @@
-package jerklib.events;
-
-/**
- * Created: Mar 10, 2008 1:38:49 AM
- *
- * @author <a href="mailto:robby.oconnor@gmail.com">Robert O'Connor</a>
- */
-public interface DCCEvent extends IRCEvent {
-}
