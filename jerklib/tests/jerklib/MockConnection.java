d7d582e79c15f526834c69364ba0529c5c60e85c
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-13 20:51:51 +0000

    I hate "//TODO" crap eclipse puts for stubs
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@606 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/tests/jerklib/MockConnection.java
+++ jerklib/tests/jerklib/MockConnection.java
@@ -10,8 +10,7 @@ public class MockConnection extends Connection
 
     MockConnection(ConnectionManager manager, SocketChannel socChannel, Session session)
     {
-        super(manager, socChannel, session);
-        // TODO Auto-generated constructor stub
+        super(manager, socChannel, session);        
     }
 
 }
