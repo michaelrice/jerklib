a57b61ce7775d896c815d469967c9593a6d46b04
anktastic <anktastic@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-16 12:53:10 +0000

    Added utilities to parse to/from numeric IP Addresses.
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@630 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/src/jerklib/util/InetAddressUtils.java
@@ -0,0 +1,53 @@
+package jerklib.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Utility functions related to IP Addresses.
+ * 
+ * @author Andres N. Kievsky
+ */
+public class InetAddressUtils {
+
+	/**
+	 * Given an ip in a numeric, string format, return the InetAddress.
+	 * @param ip the ip address in string format (such as 3232235780)
+	 * @return the InetAddress object (such as the object representing 192.168.1.4)
+	 */
+	public static InetAddress parseNumericIp(String ip)
+	{
+		return parseNumericIp(Long.parseLong(ip));
+	}
+
+	/**
+	 * Given an ip in numeric format, return the InetAddress.
+	 * @param ip the ip address in long (such as 3232235780)
+	 * @return the InetAddress object (such as the object representing 192.168.1.4)
+	 */
+	public static InetAddress parseNumericIp(long ip)
+	{
+	        try {
+				return InetAddress.getByAddress(numericIpToByteArray(ip));
+			} catch (UnknownHostException e) {
+				return null;
+			}
+	}
+
+	/**
+	 * Given an ip in numeric format, return a byte array that can be fed to
+	 * InetAddress.
+	 * @param ip the ip address in long (such as 3232235780)
+	 * @return the byte array.
+	 */
+	public static byte[] numericIpToByteArray(long ip)
+	{
+        byte[] ipArray = new byte[4];
+        ipArray[3] = (byte) (ip & 255);
+        ipArray[2] = (byte) ((ip >> 8) & 255);
+        ipArray[1] = (byte) ((ip >> 16) & 255);
+        ipArray[0] = (byte) ((ip >> 24) & 255);
+        return ipArray;
+	}
+
+}
