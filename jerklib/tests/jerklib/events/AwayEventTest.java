7569837193aa3b707ef992d75f7560bcec0be85a
r0bsf <r0bsf@c1803e86-cc43-0410-955d-caceda15018b>
2008-03-19 04:34:51 +0000

    just commiting this so i dont lose it when i delete/recheck out my tree
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@647 c1803e86-cc43-0410-955d-caceda15018b

---
+++ jerklib/tests/jerklib/events/AwayEventTest.java
@@ -0,0 +1,52 @@
+package jerklib.events;
+
+import jerklib.EventTestBase;
+import jerklib.tasks.TaskImpl;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Created: Mar 18, 2008 9:50:23 PM
+ *
+ * @author <a href="mailto:robby.oconnor@gmail.com">Robert O'Connor</a>
+ */
+public class AwayEventTest extends EventTestBase
+{
+	private List<AwayEvent> events = new ArrayList<AwayEvent>();
+
+	public AwayEventTest()
+	{
+        super("/away.data", System.getProperty("user.home") + File.separator + "jerklib.tests.user.ouput");        
+    }
+
+	@BeforeTest
+	public void init()
+	{
+		createSession();
+		addServerInfo(ServerInfo.ALL);
+		session.onEvent(new TaskImpl("away")
+		{
+			public void receiveEvent(IRCEvent e)
+			{
+				events.add((AwayEvent)e);
+			}
+        }, IRCEvent.Type.AWAY_EVENT);
+
+		conMan.start(session);
+	}
+
+	@Test
+	public void testBahamutUserWentAway()
+	{
+		AwayEvent event = events.get(1);
+        assertTrue(event.isYou());
+        assertTrue(event.isAway());
+        
+    }	
+}
