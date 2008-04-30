e665a779038e3a7ea4f93e56798fb7b6e0de62b9
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-04-30 03:34:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@688 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/examples/Example.java
+++ jerklib/src/jerklib/examples/Example.java
@@ -2,29 +2,24 @@ package jerklib.examples;
 
 import jerklib.ConnectionManager;
 import jerklib.ProfileImpl;
-import jerklib.ServerInformation;
-import jerklib.ServerInformation.ModeType;
 import jerklib.Session;
 import jerklib.events.*;
 import jerklib.events.IRCEvent.Type;
 import jerklib.events.listeners.IRCEventListener;
 import jerklib.tasks.TaskImpl;
 
-import java.util.logging.Logger;
-
 /**
  * @author mohadib A simple example that demonsrates how to use JerkLib
  */
 public class Example implements IRCEventListener
 {
-    Logger log = Logger.getLogger(this.getClass().getName());
     private ConnectionManager manager;
 
     public Example()
     {
         /*
            * ConnectionManager takes a Profile to use for new connections. The profile
-           * will contain the users nick , real name , alt. nick 1 and. alt nick 2
+           * will contain the users name , nick , alt. nick 1 and. alt nick 2
            */
         manager = new ConnectionManager(new ProfileImpl("scripy", "dibz", "dibz_", "dibz__"));
 
@@ -66,104 +61,23 @@ public class Example implements IRCEventListener
       */
     public void receiveEvent(IRCEvent e)
     {
-        if (e.getType() == Type.CHANNEL_MESSAGE)
-        {
-
-            MessageEvent me = (MessageEvent) e;
-            if (me.getMessage().equals("*server info"))
-            {
-                ServerInformation info = e.getSession().getServerInformation();
-                me.getChannel().say("Name:" + info.getServerName());
-                me.getChannel().say("IRCD:" + info.getIrcdString());
-                me.getChannel().say("CaseMapping:" + info.getCaseMapping());
-
-                String modes = "";
-
-                for (String mode : info.getModes(ModeType.ALL))
-                {
-                    modes += mode;
-                }
-                me.getChannel().say("Supported Modes:" + modes);
-            }
-        }
-        else if (e.getType() == Type.CONNECT_COMPLETE)
+        if (e.getType() == Type.CONNECT_COMPLETE)
         {
             /* connection to server is complete */
-            log.info("Joining");
-            // e.getSession().joinChannel("#sand-irc");
             e.getSession().join("#jerklib");
-            // e.getSession().joinChannel("#ubuntu");
-            // e.getSession().joinChannel("#debian");
-            // e.getSession().joinChannel("#perl");
-        }
-        if (e.getType() == Type.NICK_IN_USE)
-        {
-            NickInUseEvent niu = (NickInUseEvent) e;
-            log.info("Nick In Use " + niu.getInUseNick());
         }
         else if (e.getType() == Type.CHANNEL_MESSAGE)
         {
-            MessageEvent me = (MessageEvent) e;
-            if (me.getMessage().equals("*server info"))
-            {
-                ServerInformation info = e.getSession().getServerInformation();
-                me.getChannel().say("Name:" + info.getServerName());
-                me.getChannel().say("IRCD:" + info.getIrcdString());
-                me.getChannel().say("CaseMapping:" + info.getCaseMapping());
-
-                String modes = "";
-
-                for (String mode : info.getModes(ModeType.ALL))
-                {
-                    modes += mode;
-                }
-                me.getChannel().say("Supported Modes:" + modes);
-            }
-            else
-            {
-                System.err.println("NO MATCH " + me.getMessage());
-            }
-        }
-        else if (e.getType() == Type.SERVER_INFORMATION)
-        {
-            ServerInformationEvent se = (ServerInformationEvent) e;
-            ServerInformation info = se.getServerInformation();
-            System.err.println("IRCD :" + info.getIrcdString());
-            System.err.println("Name :" + info.getServerName());
-            System.err.println("Case Mapping :" + info.getCaseMapping());
-            System.err.println("Max Chan Name :" + info.getMaxChannelNameLength());
-
-            for (String s : info.getChannelPrefixes())
-            {
-                log.info("Prefix:" + s);
-            }
-
-        }
-        else if (e.getType() == Type.TOPIC)
-        {
-            log.info("TOPIC EVENT");
+        	MessageEvent me = (MessageEvent)e;
+        	System.out.println(me.getNick() + ":" + me.getMessage());
         }
         else if (e.getType() == Type.JOIN_COMPLETE)
         {
-            log.info("JOIN COMPLETE");
             JoinCompleteEvent jce = (JoinCompleteEvent) e;
             if (jce.getChannel().getName().equals("#jerklib"))
             {
                 /* say hello and version number */
-                e.getSession().notice(jce.getChannel().getName(), "Hello from Jerklib " + ConnectionManager.getVersion());
-            }
-        }
-        else if (e.getType() == Type.MODE_EVENT)
-        {
-            ModeEvent me = (ModeEvent) e;
-            log.info("MODE Set By:" + me.setBy());
-            log.info("MODE Channel:" + me.getChannel().getName());
-            for (String mode : me.getModeMap().keySet())
-            {
-                for (String target : me.getModeMap().get(mode))
-                {
-                    log.info("MODE " + mode + "->" + target);
-                }
+               jce.getChannel().say("Hello from Jerklib " + ConnectionManager.getVersion());
             }
         }
         else
