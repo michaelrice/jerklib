e5425fdb9f4c018d7ea41d197513e1ace8024eef
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-05-13 14:01:54 +0000

    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@741 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/util/IdentServer.java
+++ jerklib/src/jerklib/util/IdentServer.java
@@ -8,13 +8,12 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 //http://books.google.com/books?id=MbHAnBh9AqQC&pg=PA310&lpg=PA310&dq=irc+fake+ident&source=web&ots=c5sHoXuzFS&sig=ZOuAeIFxKUYanirnj_hbnfpCXBQ&hl=en#PPA311,M1
 public class IdentServer implements Runnable
 {
 	private ServerSocket socket;
 	private String login;
-	
+
 	public IdentServer(String login)
 	{
 		this.login = login;
@@ -23,41 +22,40 @@ public class IdentServer implements Runnable
 			socket = new ServerSocket(113);
 			socket.setSoTimeout(60000);
 		}
-		catch (Exception e) 
+		catch (Exception e)
 		{
 			// log
 			e.printStackTrace();
 		}
 		new Thread(this).start();
 	}
-	
-	
+
 	public void run()
 	{
-		if(socket == null) return;
+		if (socket == null) return;
 		try
 		{
 			Socket soc = socket.accept();
 			soc.setSoTimeout(60000);
-			
+
 			BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
-      
-      String line = reader.readLine();
-      if (line != null) 
-      {
-          line = line + " : USERID : UNIX : " + login;
-          writer.write(line + "\r\n");
-          writer.flush();
-          writer.close();
-      }
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+
+			String line = reader.readLine();
+			if (line != null)
+			{
+				writer.write(line + " : USERID : UNIX : " + login + "\r\n");
+				writer.flush();
+				writer.close();
+				reader.close();
+			}
 			socket.close();
 		}
 		catch (IOException e)
 		{
-			///log
+			// /log
 			e.printStackTrace();
 		}
-	
+
 	}
 }
