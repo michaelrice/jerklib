e58f644659e4877a7bf7a821dca4abebaaac6d2c
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-05-18 07:37:58 +0000

    chaning parser , jerklib will be broken for a while , be ware
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@748 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/tokens/EventToken.java
+++ jerklib/src/jerklib/tokens/EventToken.java
@@ -1,118 +1,145 @@
 package jerklib.tokens;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import jerklib.tokens.Token.Type;
+/**
+ * 
+ * A Class to parse a line of IRC text
+ * 
+<pre>
+<message>  ::= [':' <prefix> <SPACE> ] <command> <params> <crlf>
+<prefix>   ::= <servername> | <nick> [ '!' <user> ] [ '@' <host> ]
+<command>  ::= <letter> { <letter> } | <number> <number> <number>
+<SPACE>    ::= ' ' { ' ' }
+<params>   ::= <SPACE> [ ':' <trailing> | <middle> <params> ]
 
-public class EventToken implements Iterable<Token>
+<middle>   ::= <Any *non-empty* sequence of octets not including SPACE
+               or NUL or CR or LF, the first of which may not be ':'>
+<trailing> ::= <Any, possibly *empty*, sequence of octets not including
+                 NUL or CR or LF>
+
+<crlf>     ::= CR LF
+</pre>
+ * 
+ * 
+ * @author mohadib
+ *
+ */
+public class EventToken
 {
-	private String data;
-	private List<Token>tokens = new ArrayList<Token>();
+	private String data , prefix = "" , command = "";
+	private List<String>arguments = new ArrayList<String>();
+	private int offset = 0;
 	
 	public EventToken(String data)
 	{
 		this.data = data;
-		tokenize();
+		parse();
+	}
+	
+	private void parse()
+	{
+		//see if message has prefix
+		if(data.startsWith(":"))
+		{
+			extractPrefix(data);
+			incTillChar();
+		}
+		
+		//get command
+		command = data.substring(offset , data.indexOf(" ", offset));
+		offset += command.length();
+		incTillChar();
+		
+		extractArguments();
+	}
+	
+	
+	private void extractArguments()
+	{
+		String argument = "";
+		for(int i = offset; i < data.length() ; i++)
+		{
+			if(!Character.isWhitespace(data.charAt(i)))
+			{
+				argument += data.charAt(i);
+				
+				//if argument.equals(":") then arg is everything till EOL
+				if(argument.length() == 1 && argument.equals(":"))
+				{
+					argument = data.substring(i + 1);
+					arguments.add(argument);
+					return;
+				}
+				offset++;
+			}
+			else
+			{
+				if(argument.length() > 0)
+				{
+					arguments.add(argument);
+					argument = "";
+				}
+				offset++;
+			}
+		}
+		
+		if(argument.length() != 0)
+		{
+			arguments.add(argument);
+		}
+	}
+	
+	// increment offset till next non whitespace char
+	private void incTillChar()
+	{
+		for(int i = offset ; i < data.length(); i++)
+		{
+			if(!Character.isWhitespace(data.charAt(i)))
+			{
+				return;
+			}
+			offset++;
+		}
+	}
+	
+	private void extractPrefix(String data)
+	{
+		//remove :
+		data = data.substring(1);
+		//move offset up one for char we removed
+		offset++;
+		
+		//set prefix
+		prefix = data.substring(0 , data.indexOf(" "));
+		
+		//increment offset
+		offset += prefix.length();
+	}
+
+	public String getPrefix()
+	{
+		return prefix;
 	}
 	
 	public String getCommand()
 	{
-		return getWordTokens().get(1).data;
+		return command;
 	}
 	
+	public List<String>getArguments()
+	{
+		return arguments;
+	}
 	
 	public String getData()
 	{
 		return data;
 	}
 	
-	public List<Token> getTokens()
-	{
-		return new ArrayList<Token>(tokens);
-	}
-	
-	public List<Token> getWordTokens()
-	{
-		List<Token> returnList = new ArrayList<Token>();
-		for(Token t : tokens)
-		{
-			if(t.type == Type.NON_WHITESPACE)
-			{
-				returnList.add(t);
-			}
-		}
-		return returnList;
-	}
-	
-	public String substring(int offset , int length)
-	{
-		return data.substring(offset , offset + length);
-	}
-	
-	public String substring(int offset)
-	{
-		return data.substring(offset);
-	}
-	
-	public String concatTokens(int tokenOffset)
-	{
-		String data = "";
-		for(int i = tokenOffset; i < tokens.size(); i++)
-		{
-			data += tokens.get(i).data;
-		}
-		return data;
-	}
-	
-	private void tokenize()
-	{
-		char[] chars = data.toCharArray();
-		
-		Token token = new Token();
-		
-		for(int i = 0; i < chars.length ; i++)
-		{
-			
-			char c = chars[i];
-			if(Character.isWhitespace(c))
-			{
-				if(token.type == null)token.type = Type.WHITESPACE;
-				if(token.type != Type.WHITESPACE)
-				{
-					tokens.add(token);
-					token = new Token();
-					token.type = Type.WHITESPACE;
-					token.offset = i;
-				}
-			}
-			else
-			{
-				if(token.type == null)token.type = Type.NON_WHITESPACE;
-				if(token.type != Type.NON_WHITESPACE)
-				{
-					tokens.add(token);
-					token = new Token();
-					token.type = Type.NON_WHITESPACE;
-					token.offset = i;
-				}
-			}
-			
-			token.len++;
-			token.data += c;
-			
-			if(i + 1 == chars.length) tokens.add(token);
-		}
-	}
-	
 	public String toString()
 	{
 		return data;
-	}
-
-	public Iterator<Token> iterator()
-	{
-		return tokens.iterator();
 	}
 }
---
+++ jerklib/tests/jerklib/TmpEventTokenTest.java
@@ -0,0 +1,51 @@
+package jerklib;
+
+import jerklib.tokens.EventToken;
+
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class TmpEventTokenTest
+{
+	@Test
+	public void parseTest1()
+	{
+		EventToken eventToken = new EventToken(":kubrick.freenode.net 353 mohadib_ = ##java :m0` derjohn mr_daniel Dry4d shadowarts victori_ erikwt Advant skoptelov _W_ Sargun inimesekene TdC_VgA df00z prgrmr shabble fex[ delskorch Ragnor blbrown paddax vlii pa kadams ntolo hashman beasty rollins lataffe xea Zaph0d^ amitev Kwitschibo wyvern` fez Fasga HelloMeow oxi bimbo dequeued jlindsay ferret_0567 ordain meeper Apocalisp EnginA pr3d4t0r traxy joed RedDisc NoReGreT Paks vinse linxeh Rytmis goki_work pithen savetheWorld cofeineSunshine" );
+		assertTrue(eventToken.getPrefix() , eventToken.getPrefix().equals("kubrick.freenode.net"));
+		
+		assertTrue(eventToken.getCommand().equals("353"));
+		
+		assertTrue(eventToken.getArguments().size()+"" , eventToken.getArguments().size() == 4);
+	}
+	
+	
+	
+	@Test
+	public void parseTest2()
+	{
+		EventToken eventToken = new EventToken(":NeWtoz!jimmy@nix-2F996C9F.dhcp.aldl.mi.charter.com PRIVMSG #tvtorrents :I should be in bed, so I'm going to bed");
+		assertTrue(eventToken.getPrefix() , eventToken.getPrefix().equals("NeWtoz!jimmy@nix-2F996C9F.dhcp.aldl.mi.charter.com"));
+		
+		assertTrue(eventToken.getCommand().equals("PRIVMSG"));
+		
+		assertTrue(eventToken.getArguments().size()+"" , eventToken.getArguments().size() == 2);
+	}
+	
+	@Test
+	public void parseTest3()
+	{
+		EventToken eventToken = new EventToken("PING :irc.nixgeeks.com");
+		assertTrue(eventToken.getPrefix() , eventToken.getPrefix().equals(""));
+		
+		assertTrue(eventToken.getCommand().equals("PING"));
+		
+		assertTrue(eventToken.getArguments().size()+"" , eventToken.getArguments().size() == 1);
+		assertTrue(eventToken.getArguments().get(0).equals("irc.nixgeeks.com"));
+	}
+	
+	
+	
+	
+	
+	
+}
