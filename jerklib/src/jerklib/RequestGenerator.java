b1a8473bc91da1f1d9049ef6f2a1fa61c0af84d8
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-05-20 02:22:16 +0000

    javadocs for RequestGenerator
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@785 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/RequestGenerator.java
+++ jerklib/src/jerklib/RequestGenerator.java
@@ -1,5 +1,13 @@
 package jerklib;
 
+/**
+ * A class for writing Session level events. 
+ * By that I mean not Channel level stuff , though
+ * some methods can be passed a Channel or channel name as a target.
+ * 
+ * @author mohadib
+ *
+ */
 class RequestGenerator
 {
 	private Connection con;
@@ -9,31 +17,70 @@ class RequestGenerator
 		this.con = con;
 	}
 
+	/**
+	 * Send Who request 
+	 * 
+	 * @param who
+	 */
+	public void who(String who)
+	{
+		con.addWriteRequest(new WriteRequest("WHO " + who, con));
+	}
+	
+	/**
+	 * Send a whois query
+	 * 
+	 * @param nick - target of whois
+	 */
 	public void whois(String nick)
 	{
 		con.addWriteRequest(new WriteRequest("WHOIS " + nick, con));
 	}
 
+	/**
+	 * Send WhoWas query
+	 * @param nick
+	 */
+	public void whoWas(String nick)
+	{
+		con.addWriteRequest(new WriteRequest("WHOWAS " + nick, con));
+	}
+	
+	/**
+	 * Invite a user to a channel
+	 * 
+	 * @param nick
+	 * @param chan
+	 */
 	public void invite(String nick, Channel chan)
 	{
 		con.addWriteRequest(new WriteRequest("INVITE " + nick + " " + chan.getName(), con));
 	}
 
+	/**
+	 * Get a List of Channels from server.
+	 */
 	public void chanList()
 	{
 		con.addWriteRequest(new WriteRequest("LIST", con));
 	}
 
+	
+	/**
+	 * Get information on a secific channel
+	 * 
+	 * @param channel
+	 */
 	public void chanList(String channel)
 	{
 		con.addWriteRequest(new WriteRequest("LIST " + channel, con));
 	}
 
-	public void whoWas(String nick)
-	{
-		con.addWriteRequest(new WriteRequest("WHOWAS " + nick, con));
-	}
-
+	/**
+	 * Join a Channel
+	 * 
+	 * @param channel
+	 */
 	public void join(String channel)
 	{
 		if (con != null)
@@ -42,6 +89,12 @@ class RequestGenerator
 		}
 	}
 	
+	/**
+	 * Join a password protected Channel
+	 * 
+	 * @param channel
+	 * @param pass
+	 */
 	public void join(String channel, String pass)
 	{
 		if (con != null)
@@ -50,6 +103,11 @@ class RequestGenerator
 		}
 	}
 	
+	/**
+	 * Send a ctcp request
+	 * @param target
+	 * @param request
+	 */
 	public void ctcp(String target, String request)
 	{
 		if (con != null)
@@ -58,61 +116,114 @@ class RequestGenerator
 		}
 	}
 
+	
+	/**
+	 * Send a notice
+	 * 
+	 * @param target
+	 * @param msg
+	 */
 	public void notice(String target, String msg)
 	{
 		con.addWriteRequest(new WriteRequest("NOTICE " + target + " :" + msg, con));
 	}
 
-	public void who(String who)
-	{
-		con.addWriteRequest(new WriteRequest("WHO " + who, con));
-	}
-
+	/**
+	 * Set self away
+	 * 
+	 * @param message
+	 */
 	public void setAway(String message)
 	{
 		con.addWriteRequest(new WriteRequest("AWAY :" + message, con));
 	}
 
+	/**
+	 * Unset away
+	 * 
+	 */
 	public void unSetAway()
 	{
 		con.addWriteRequest(new WriteRequest("AWAY", con));
 	}
 
+	/**
+	 * Send server version query
+	 */
 	public void getServerVersion()
 	{
 		con.addWriteRequest(new WriteRequest("VERSION " + con.getHostName(), con));
 	}
 
+	/**
+	 * Send server version query for specific hostmask pattern
+	 * 
+	 * @param hostPattern
+	 */
 	public void getServerVersion(String hostPattern)
 	{
 		con.addWriteRequest(new WriteRequest("VERSION " + hostPattern, con));
 	}
 
+	/**
+	 * Send nick change request
+	 * 
+	 * @param nick
+	 */
 	public void changeNick(String nick)
 	{
 		con.addWriteRequest(new WriteRequest("NICK " + nick, con));
 	}
 
+	/**
+	 * Set a mode
+	 * 
+	 * @param target
+	 * @param mode
+	 */
 	public void mode(String target , String mode)
 	{
 		con.addWriteRequest(new WriteRequest("MODE " + target + " " + mode, con));
 	}
 
+	/**
+	 * Send ctcp action
+	 * 
+	 * @param target
+	 * @param actionText
+	 */
 	public void action(String target, String actionText)
 	{
 		ctcp(target, actionText);
 	}
 
+	/**
+	 * Speak in a channel
+	 * @param msg
+	 * @param channel
+	 * @see Channel#say(String)
+	 */
 	public void sayChannel(String msg, Channel channel)
 	{
 		con.addWriteRequest(new WriteRequest(msg, channel, con));
 	}
 
+	/**
+	 * Send a private message
+	 * 
+	 * @param nick
+	 * @param msg
+	 */
 	public void sayPrivate(String nick, String msg)
 	{
 		con.addWriteRequest(new WriteRequest(msg, con, nick));
 	}
 
+	/**
+	 * Send raw text to server
+	 * 
+	 * @param data
+	 */
 	public void sayRaw(String data)
 	{
 		con.addWriteRequest(new WriteRequest(data, con));
