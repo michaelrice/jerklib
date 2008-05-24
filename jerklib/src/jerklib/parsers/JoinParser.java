4d8c3c775d57c53488c9c18f1c452ffac8145a41
mohadib_ <mohadib_@c1803e86-cc43-0410-955d-caceda15018b>
2008-05-24 02:23:39 +0000

    bug fix in joinparser
    git-svn-id: svn://svn.code.sf.net/p/jerklib/code@820 c1803e86-cc43-0410-955d-caceda15018b

--- jerklib/src/jerklib/parsers/JoinParser.java
+++ jerklib/src/jerklib/parsers/JoinParser.java
@@ -11,27 +11,14 @@ public class JoinParser implements CommandParser
 
 	// :r0bby!n=wakawaka@guifications/user/r0bby JOIN :#jerklib
 	// :mohadib_!~mohadib@68.35.11.181 JOIN &test
-	
+
 	public IRCEvent createEvent(IRCEvent event)
 	{
 		Session session = event.getSession();
-		
-		JoinEvent je = new JoinEvent
-		(
-			event.getRawEventData(), 
-			session, 
-			session.getChannel(event.arg(0)) // channel
-		);
-		
-		if(je.getNick().equalsIgnoreCase(event.getSession().getNick()))
-		{
-			return new JoinCompleteEvent
-			(
-				je.getRawEventData(), 
-				je.getSession(),
-				new Channel(je.getChannelName() , event.getSession())
-			);
-		}
-		return je;
+
+		if (!event.getNick().equalsIgnoreCase(event.getSession().getNick())) { return new JoinEvent(event.getRawEventData(), session, session.getChannel(event.arg(0)) // channel
+		); }
+
+		return new JoinCompleteEvent(event.getRawEventData(), event.getSession(), new Channel(event.arg(0), event.getSession()));
 	}
 }
